4f334fedf6d3c1f0cee400cf504c25662a4ad610
Ethan Metz <76500833@protonmail.com>
2024-04-11 10:30:02 -0700

    commented out typeDefs from server

--- server/server.js
+++ server/server.js
@@ -9,8 +9,8 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  // typeDefs,
+  // resolvers,
 });
 
 const startApolloServer = async () => {
