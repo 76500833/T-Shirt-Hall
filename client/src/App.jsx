8635f3ca522d7f1bd28e99f21ec9468565d9a705
Ethan Metz <76500833@protonmail.com>
2024-04-12 20:06:02 -0700

    removed a test component and redid home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
-
+import Home from './components/home';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 const client = new ApolloClient({
   uri: 'http://localhost:3001/graphql', 
   cache: new InMemoryCache()
 });
 import './App.css'
-import Home from './components/home';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <ApolloProvider client={client}>
+
       < Home/>
       
       </ApolloProvider>
