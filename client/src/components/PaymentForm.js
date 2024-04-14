00b314e0bb1359a024e5cdc735d73db81bb96da0
unknown <gabebarraza33@gmail.com>
2024-04-13 22:16:26 -0500

    Finishing the paymentform js for now on the client side.

--- client/src/components/PaymentForm.js
+++ client/src/components/PaymentForm.js
@@ -1,9 +1,30 @@
-import React, {useState} from 'react'
-import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
-import axios from 'axios'
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js"
+import axios from "axios"
+import React, { useState } from 'react'
 
-function PaymentForm() {
-    const [success, setSuccess] = useState(false)
+
+const CARD_OPTIONS = {
+	iconStyle: "solid",
+	style: {
+		base: {
+			iconColor: "#c4f0ff",
+			color: "#fff",
+			fontWeight: 500,
+			fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
+			fontSize: "16px",
+			fontSmoothing: "antialiased",
+			":-webkit-autofill": { color: "#fce883" },
+			"::placeholder": { color: "#87bbfd" }
+		},
+		invalid: {
+			iconColor: "#ffc7ee",
+			color: "#ffc7ee"
+		}
+	}
+}
+
+export default function PaymentForm() {
+    const [success, setSuccess ] = useState(false)
     const stripe = useStripe()
     const elements = useElements()
 
@@ -14,7 +35,7 @@ function PaymentForm() {
             type: "card",
             card: elements.getElement(CardElement)
         })
-    
+
 
     if(!error) {
         try {
@@ -23,24 +44,37 @@ function PaymentForm() {
                 amount: 1000,
                 id
             })
-            
+
             if(response.data.success) {
-                console.log("Successful payment!");
+                console.log("Successful payment")
                 setSuccess(true)
             }
 
         } catch (error) {
-            console.log("Error :(", error);
+            console.log("Error", error)
         }
     } else {
-        console.log(error.message);
+        console.log(error.message)
     }
 }
-    
 
     return (
-        
+        <>
+        {!success ? 
+        <form onSubmit={handleSubmit}>
+            <fieldset className="FormGroup">
+                <div className="FormRow">
+                    <CardElement options={CARD_OPTIONS}/>
+                </div>
+            </fieldset>
+            <button>Pay</button>
+        </form>
+        :
+       <div>
+           <h2>Swag has been acquired!</h2>
+       </div> 
+        }
+            
+        </>
     )
 }
-
-export default PaymentForm
