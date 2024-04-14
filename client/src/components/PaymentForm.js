1bbfd28c0011b984f79eb39840e45212557cce0c
unknown <gabebarraza33@gmail.com>
2024-04-13 22:13:58 -0500

    Payment form functions

---
+++ client/src/components/PaymentForm.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
+import axios from 'axios'
+
+function PaymentForm() {
+    const [success, setSuccess] = useState(false)
+    const stripe = useStripe()
+    const elements = useElements()
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const {error, paymentMethod} = await stripe.createPaymentMethod({
+            type: "card",
+            card: elements.getElement(CardElement)
+        })
+    
+
+    if(!error) {
+        try {
+            const {id} = paymentMethod
+            const response = await axios.post("http://localhost:4000/payment", {
+                amount: 1000,
+                id
+            })
+            
+            if(response.data.success) {
+                console.log("Successful payment!");
+                setSuccess(true)
+            }
+
+        } catch (error) {
+            console.log("Error :(", error);
+        }
+    } else {
+        console.log(error.message);
+    }
+}
+    
+
+    return (
+        
+    )
+}
+
+export default PaymentForm
