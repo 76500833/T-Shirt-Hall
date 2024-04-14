70747d09c167659fa073d5f669c1f3e5824cba40
unknown <gabebarraza33@gmail.com>
2024-04-13 22:23:14 -0500

    changes to stripe container

--- client/src/components/StripeContainer.js
+++ client/src/components/StripeContainer.js
@@ -1,17 +1,16 @@
-import React from 'react'
-import { Elements } from '@stripe/react-stripe-js'
-import { loadStripe } from '@stripe/stripe-js'
-
+import { Elements } from "@stripe/react-stripe-js"
+import { loadStripe } from "@stripe/stripe-js"
+import React from "react"
+import PaymentForm from "./PaymentForm"
 
 const PUBLIC_KEY = "pk_test_51P5Ix2FU6mSlgK5VHmg90blNc166by70jmmPri5ggfDN5UdIuybBMRqong9n3jgeBrODlzJRqMUfq8vZaW1JUuAA00Hd3lVkGE"
+
 const stripeTestPromise = loadStripe(PUBLIC_KEY)
 
-function StripeContainer() {
+export default function StripeContainer() {
     return (
         <Elements stripe={stripeTestPromise}>
             <PaymentForm />
         </Elements>
     )
 }
-
-export default StripeContainer
