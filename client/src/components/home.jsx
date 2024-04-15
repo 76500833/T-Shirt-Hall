0fb19676b8afb7640662b0c231d3c8116ce16b90
Ethan Metz <76500833@protonmail.com>
2024-04-14 19:02:41 -0700

    switched up the home page  abit

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_SHIRTS } from '../utils/query';
+import { createCart, addToCart } from '../utils/query.js';
 
 function Home() {
 
@@ -17,24 +18,27 @@ function Home() {
   const mapOfShirts = shirt.map((shirt, index) => (
     // per shirt insert a card
     <div className="card w-96 bg-base-100 shadow-xl" style={{ border: "1px solid black", marginBottom: "15px" }}>
-    //Image of shirt.
       <figure>
         <img src={`/images/${shirt.image}`} alt={shirt.name} style={{ width: "100%" }} />
+
       </figure>
       <select
-      // Stylize the card
+        onChange={(e) => setSelectedSize(e.target.value)}
+        // Stylize the card
         style={{
           padding: "10px",
-          height: "44px",
+          height: "38px",
           borderTopLeftRadius: "0",
           borderTopRightRadius: "0",
           borderBottomLeftRadius: "10px",
           borderBottomRightRadius: "10px",
-          color: "white"
+          color: "white",
+          width: "100%",
+          fontSize: "medium"
         }}
 
         // Size dropdown
-        className="bg-gradient-to-r from-blue-900 to-blue-500 row-start-1 col-start-1 bg-slate-50 dark:bg-slate-800">
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
         //text in the undropped dropdown.
         <option value="" selected disabled hidden>Select a Size</option>
         //dropdown text
@@ -43,28 +47,44 @@ function Home() {
         <option>Large</option>
       </select>
 
-      //Add to cart button.
-      <div style={{ display: "flex", gap: "20px", margin: "10px" }}>
-        <div class="relative z-0" >
-          <button style={{ color: "white", padding: "10px", borderRadius: "10px", marginLeft: "10px", marginBottom: "5px" }} className="bg-gradient-to-r from-green-700 to-green-500"> Add to Cart</button>
+      <div style={{ display: "flex", gap: "20px", margin: "10px", }}>
+
+
+        <div className="relative z-0" style={{ width: "100%" }} >
+          <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800" onClick={() => {
+            if (selectedSize) {
+              setCart([...cart, { title: shirt.title, size: selectedSize }]);
+              alert(`Size ${selectedSize} selected, added ${shirt.title} to cart`);
+            } else {
+              alert('No size selected');
+            }
+          }}>
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              Add to Cart
+            </span>
+          </button>          <div className="badge badge-outline text-white" style={{ position: "absolute", right: "10px" }}>$15</div>
         </div>
       </div>
     </div>
-  
+
   ));
+
+
+
+
+
+
+
+
 
   //display the fallowing in the page
   return (
     <>
-    //header
       <span style={{ display: "flex", justifyContent: "center" }}>
         <h1 class="text-5xl font-extrabold dark:text-white" style={{ marginBottom: "25px" }}>T Shirt Hall</h1>
       </span>
 
-      //style the layout of the shirts
       <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
-        
-        //content of map of shirts from above
         {mapOfShirts}
       </div>
     </>
