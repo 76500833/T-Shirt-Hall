760c3a5cd6ccb883401117414c1f100025b2514a
Ethan Metz <76500833@protonmail.com>
2024-04-14 20:58:56 -0700

    sorry emily

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_SHIRTS } from '../utils/query';
-import { createCart, addToCart } from '../utils/query.js';
+
 
 function Home() {
 
