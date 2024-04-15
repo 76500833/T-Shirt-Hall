d700767359b4d86773e28c6da48834cda75e63e9
emily shea <emilyshea947@gmail.com>
2024-04-14 21:50:42 -0400

    added font

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Home from './components/Home';
 import DroopyImage from './components/droopyImage'
+import Faq from './components/faq'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 const client = new ApolloClient({
   uri: 'http://localhost:3001/graphql', 
@@ -12,9 +13,9 @@ function App() {
 
   return (
     <ApolloProvider client={client}>
-  
+  < DroopyImage />
       < Home/>
-      
+      < Faq />
       </ApolloProvider>
     
   )
--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -81,7 +81,7 @@ function Home() {
   return (
     <>
       <span style={{ display: "flex", justifyContent: "center" }}>
-      <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">T-Shirt Hall</span> Scalable AI.</h1>
+      <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">T-Shirt Hall</span></h1>
 
       </span>
 
