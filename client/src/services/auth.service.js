945da5b41fa2c7580468a1312b0916e3c14fb3d9
unknown <gabebarraza33@gmail.com>
2024-04-15 19:28:20 -0500

    beginning the post requests etc for sign up and login

---
+++ client/src/services/auth.service.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+
+const API_URL = "/auth";
+
+
+//signup
+const signup = (email, password) => {
+  return axios
+    .post(API_URL + "/signup", {
+      email,
+      password,
+    })
+    .then((response) => {
+      if (response.data.accessToken) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+
+      return response.data;
+    });
+};
+
+
+//login
+const login = (email, password) => {
+  return axios
+    .post(API_URL + "/login", {
+      email,
+      password,
+    })
+    .then((response) => {
+      if (response.data.accessToken) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+
+      return response.data;
+    });
+};
+
+
+//logout
+const logout = () => {
+  localStorage.removeItem("user");
+};
+
+
+const getCurrentUser = () => {
+  return JSON.parse(localStorage.getItem("user"));
+};
+
+const authService = {
+  signup,
+  login,
+  logout,
+  getCurrentUser,
+};
+
+export default authService;
