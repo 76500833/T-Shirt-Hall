dc30af2643f230f74ec55202d3596827ebe0d374
unknown <gabebarraza33@gmail.com>
2024-04-15 20:00:41 -0500

    Adding the functions calling the authservice in signup.

---
+++ client/src/components/Signup.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import AuthService from "../services/auth.service";
+import { useNavigate } from "react-router-dom";
+
+
+//Calls Authservice to make a User, If no user then an error is thrown.
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    try {
+      await AuthService.signup(email, password).then(
+        (response) => {
+          // check for token and user already exists with 200
+          //   console.log("Sign up successfully", response);
+          navigate("/home");
+          window.location.reload();
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+
+//What the user sees
+  return (
+    <div>
+      <form onSubmit={handleSignup}>
+        <h3>Sign up</h3>
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Sign up</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
