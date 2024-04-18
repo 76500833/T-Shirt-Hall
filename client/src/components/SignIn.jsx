c05ba2c648becdd0bc9c9c755ea1a8375753434f
emily shea <emilyshea947@gmail.com>
2024-04-18 19:48:36 -0400

    changed a color

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -43,7 +43,7 @@ function SignIn() {
                         </div>
 
                         {/* Submit button */}
-                        <button type="submit" className="text-white bg-green-400 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                        <button type="submit" className="text-white bg-blue-400 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
                     </form>
 
                     {/* Modal for authentication */}
