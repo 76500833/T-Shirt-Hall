cb9e4f9ad9af1eafcb4f93df87d0d2ea045771e0
emily shea <emilyshea947@gmail.com>
2024-04-18 20:21:32 -0400

    changed color of price

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -53,7 +53,7 @@ function Home() {
       <div style={{ display: "flex", gap: "20px", margin: "10px", }}>
 
 
-        <div className="relative z-0" style={{ width: "100%" }} >
+        <div className="relative z-0" style={{ width: "40%" }} >
           <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800" onClick={() => {
             if (selectedSize) {
               setCart([...cart, { title: shirt.title, size: selectedSize }]);
@@ -65,7 +65,7 @@ function Home() {
             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
               Add to Cart
             </span>
-          </button>          <div className="badge badge-outline text-white" style={{ position: "absolute", right: "10px" }}>$15</div>
+          </button>          <div className="badge badge-outline text-black" style={{ position: "absolute", right: "10px" }}>$15</div>
         </div>
       </div>
     </div>
