463c5984258339e78d0c21bdd4b984a3642e23b0
emily shea <emilyshea947@gmail.com>
2024-04-18 20:42:54 -0400

    added path for cart image

--- client/src/components/ShirtCart.jsx
+++ client/src/components/ShirtCart.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 function shirtCart() {
 
     return(
+        <div>
+            <img src="/path/to/your/cart-logo.png" alt="Cart Logo" />
             <h1>CART</h1>
+            </div>
     )
 }
 
 export default shirtCart
+
