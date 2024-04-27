e816cf9ef5c89dee5c40b2e5cb96dafbd005b8f9
Ethan Metz <76500833@protonmail.com>
2024-04-27 14:24:48 -0700

    auth middleware added to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,38 @@
 import { useState } from 'react'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
 import Home from './components/home';
 import DroopyImage from './components/droopyImage'
 import FaqDrawer from './components/FaqDrawer'
 import SignUp from './components/SignUp'
 import SignIn from './components/SignIn'
 import ShirtCart from './components/ShirtCart'
-
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-const client = new ApolloClient({
+//add additional data to the request as it comes in and goes out
+import { setContext } from '@apollo/client/link/context'
+const httpLink = createHttpLink({
   uri: '/graphql',
-  cache: new InMemoryCache()
 });
+const authLink = setContext((_, { headers }) => {
+  //get authentication token from local storage if its exists
+const token = localStorage.getItem('id_token')
+//return the header to the context so client can read them
+return {
+  headers: {
+    ...headers,
+    authorization: token ? `Bearer ${token}` : '',
+  },
+  };
+});
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+//jwt middleware that will attach the JWT to every request as an 'authorization' header
 import './App.css'
 function App() {
   const [isSignedUp, setIsSignedUp] = useState(false);
---
+++ client/src/utils/auth.js
@@ -0,0 +1,26 @@
+import decode from 'jwt-decode';
+class AuthService {
+    getUser() {
+        //get user data from json web token by decoding it
+        return decode(this.getToken())
+    }
+    loggedIn() {
+        //return true or false if token exists
+        const token = this.getToken();
+        return token ? true : false;
+    }
+    getToken() {
+        //retrieves user token from local storage
+        return localStorage.getItem('id_token')
+    }
+    login(idToken) {
+        //saves user token in local storage and reloads the app for logged in status to take effect
+        localStorage.setItem('id_token', idToken);
+        window.location.assign('/')
+    }
+    logout() {
+        localStorage.removeItem('id_token');
+        window.location.reload()
+    }
+}
+export default new AuthService
